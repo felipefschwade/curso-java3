894a17a10f739578982a5395bc6905ecc54529f0
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-05-13 16:58:48 -0400

    Aula 2 - Atividade 1 - Criando o JAR do programa

--- src/br/com/empresa/conta/ValorInvalidoException.java
+++ src/br/com/empresa/conta/ValorInvalidoException.java
@@ -1,6 +1,11 @@
 package br.com.empresa.conta;
 
 public class ValorInvalidoException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public ValorInvalidoException(double valor) {
 		super("O Valor" + valor + " é inválido"); //Super é utilizado para chamar o construtor da classe pai
 	}
