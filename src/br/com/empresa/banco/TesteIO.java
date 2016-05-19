d5be93657b081626b5ec78fcc1752163f5709739
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-05-19 13:50:03 -0400

    Aula 4 - Exercicio 6 - Trocando o BufferedReader por um Scanner

--- src/br/com/empresa/banco/TesteIO.java
+++ src/br/com/empresa/banco/TesteIO.java
@@ -6,17 +6,16 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class TesteIO {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(
-                new InputStreamReader(
-                    new FileInputStream("arquivo.txt")));
-		String linha = br.readLine();
-		while (linha != null) {
+		InputStream is = new FileInputStream("arquivo.txt");
+		Scanner sc = new Scanner(is);
+		while(sc.hasNextLine()) {
+			String linha = sc.nextLine();
 			System.out.println(linha);
-			linha = br.readLine();
 		}
-		br.close();
+		sc.close();
 	}
 }
