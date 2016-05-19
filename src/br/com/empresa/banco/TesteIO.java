3d7a05d13950dec307ed85af2c22922048e00b03
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-05-19 14:16:48 -0400

    Aula 4 - Exercicio 7 - Utilizando um OutputStream com o PrintStream

--- src/br/com/empresa/banco/TesteIO.java
+++ src/br/com/empresa/banco/TesteIO.java
@@ -2,20 +2,26 @@ package br.com.empresa.banco;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class TesteIO {
 	public static void main(String[] args) throws IOException {
 		InputStream is = new FileInputStream("arquivo.txt");
+		OutputStream os = new FileOutputStream("escrita.txt");
+		PrintStream ps = new PrintStream(os);
 		Scanner sc = new Scanner(is);
 		while(sc.hasNextLine()) {
 			String linha = sc.nextLine();
-			System.out.println(linha);
+			ps.println(linha);
 		}
 		sc.close();
+		ps.close();
 	}
 }
