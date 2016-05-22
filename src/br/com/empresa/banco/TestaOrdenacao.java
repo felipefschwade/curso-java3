da757edd89bba63143aad724e061c5048b6fbfbc
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-05-21 22:23:03 -0400

    Aula 5 - Atividade 7, gerando contas com números aleatórios

--- src/br/com/empresa/banco/TestaOrdenacao.java
+++ src/br/com/empresa/banco/TestaOrdenacao.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 import br.com.empresa.conta.Conta;
 import br.com.empresa.conta.ContaPoupanca;
@@ -13,8 +14,13 @@ public class TestaOrdenacao {
 		List<ContaPoupanca> contas = new LinkedList<ContaPoupanca>();
 		ContaPoupanca c1 = new ContaPoupanca(123, "Cesar");
 		ContaPoupanca c2 = new ContaPoupanca(132, "Pedro");
-		contas.add(c2);
-		contas.add(c1);
+		for (int i = 0; i < 10; i++) {
+			Random random = new Random();
+			ContaPoupanca c = new ContaPoupanca(random.nextInt(100000), "aaaaa");
+			contas.add(c);
+			System.out.println(c);
+		} 
+		System.out.println("Ordenando");
 		Collections.sort(contas);
 		for (ContaPoupanca conta : contas) {
 			System.out.println(conta);
