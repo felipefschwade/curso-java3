a18d5041dc99e76f76af0aa935e4a2fa45b9f1eb
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-05-22 13:37:48 -0400

    Aula 5 - Atividade 10 - Criando a classe testa perfomance e testando a perfomance de duas Collections

---
+++ src/br/com/empresa/banco/TestaPerfomance.java
@@ -0,0 +1,29 @@
+package br.com.empresa.banco;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+
+public class TestaPerfomance {
+	 public static void main(String[] args) {
+         System.out.println("Iniciando...");
+         Collection<Integer> teste = new HashSet<>();
+         long inicio = System.currentTimeMillis();
+
+         int total = 30000;
+
+         for (int i = 0; i < total; i++) {
+             teste.add(i);
+         }
+         long fim = System.currentTimeMillis();
+         long tempo = fim - inicio;
+         System.out.println("Tempo gasto: " + tempo);
+         inicio = System.currentTimeMillis();
+         for (int i = 0; i < total; i++) {
+             teste.contains(i);
+         }
+         fim = System.currentTimeMillis();
+         tempo = fim - inicio;
+         System.out.println("Tempo gasto: " + tempo);
+     }
+}
